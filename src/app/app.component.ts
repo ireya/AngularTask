2f23d1151cd2888559239b7e6dcbf736de84d8ab
ireya <147235454+ireya@users.noreply.github.com>
2025-02-25 16:49:54 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { ProductListComponent } from './product-list/product-list.component'; // Import the component
+import { ProductListComponent } from './product-list/product-list.component';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule], // Add ProductListComponent to the imports array
+  imports: [RouterModule], 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
